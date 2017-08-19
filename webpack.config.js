ac83758815a01969a68bcf9c1a2e45cac007793b
Henry Smith <henry@henrysmith.org>
2017-08-19 11:17:09 +0200

    Set up webpack dashboard

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,20 @@
 const path = require('path');
-const dashboard = require('webpack-dashboard/plugin');
+const Dashboard = require('webpack-dashboard');
+const DashboardPlugin = require('webpack-dashboard/plugin');
+const dashboard = new Dashboard;
 
 module.exports = {
+  devServer: {
+    contentBase: path.join(__dirname, 'src'),
+    compress: true,
+    historyApiFallback: true,
+    port: 8080,
+    host: '127.0.0.1',
+    inline: true,
+    quiet: true,   // important
+    hot: true,
+    open: true,
+  },
   entry: path.join(__dirname, 'src', 'index.js'),
   module: {
     loaders: [{
@@ -12,6 +25,12 @@ module.exports = {
   output: {
     filename: 'rgb-line.js'
   },
+  plugins: [
+    new DashboardPlugin({
+      port: 8080,
+      handler: dashboard.setData,
+    }),
+  ],
   resolve: {
     modules: [__dirname, 'node_modules'],
     extensions: ['*','.js','.jsx'],
