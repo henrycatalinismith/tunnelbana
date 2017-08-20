a6fa323ee1daae65f28dd66094d3e1b86bdb360e
Henry Catalini Smith <smith@hen.cat>
2017-08-20 11:28:14 +0200

    Add some data

--- src/reducers/connections.js
+++ src/reducers/connections.js
@@ -5,7 +5,7 @@ const initialState = [];
 export default function(state = initialState, action) {
   switch (action.type) {
     case actions.ADD_CONNECTION:
-      return [...state, [action.source, action.destination]];
+      return [...state, action.connection];
     default:
       return [...state];
   }
