4acb5e4b305e11b3919c248d71f6147354146232
Henry Smith <henry@henrysmith.org>
2017-08-20 20:12:17 +0200

    Make the train go to Gamla Stan

--- src/actions/trains.js
+++ src/actions/trains.js
@@ -1,8 +1,16 @@
 export const ADD_TRAIN = 'ADD_TRAIN';
+export const DEPART = 'DEPART';
 
 export function addTrain(train) {
   return {
     type: ADD_TRAIN,
     train,
+  };
+}
+
+export function depart(departure) {
+  return {
+    type: DEPART,
+    departure,
   };
 }
