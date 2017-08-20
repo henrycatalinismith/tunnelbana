d8bfd0645d7e534d7d8bcb094eac18f7e60a20d4
Henry Catalini Smith <henry@catalinismith.se>
2017-08-20 19:30:35 +0200

    More normalization

--- src/components/Connection.js
+++ src/components/Connection.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { line } from '../reducers/lines';
 import { station, stations } from '../reducers/stations';
 
 export class Connection extends React.Component {
   static propTypes = {
     connection: PropTypes.object,
+    line: PropTypes.object,
     map: PropTypes.object,
+    source: PropTypes.object,
+    destination: PropTypes.object,
   }
 
   render() {
@@ -27,13 +31,17 @@ export class Connection extends React.Component {
         + this.props.map.center.y
         + this.props.destination.y,
     };
+    if (isNaN(from.y)) {
+      console.log(this.props.source);
+      console.error('omg')
+    }
 
     const d = 'M' + [from.x, from.y, to.x, to.y].join(' ');
 
     return (
       <path
         d={d}
-        stroke={this.props.connection.color}
+        stroke={this.props.line.color}
         strokeWidth="8"
       />
     );
@@ -42,6 +50,7 @@ export class Connection extends React.Component {
 
 const mapStateToProps = (state, ownProps) => {
   return {
+    line: line(state.lines, ownProps.connection.lineId),
     map: state.map,
     source: station(state.stations, ownProps.connection.sourceId),
     destination: station(state.stations, ownProps.connection.destinationId),
