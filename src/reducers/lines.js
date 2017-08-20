242cbd46cff652681d129fb3a7959705a6d56789
Henry Catalini Smith <henry@catalinismith.com>
2017-08-20 19:30:35 +0200

    More normalization

--- src/reducers/lines.js
+++ src/reducers/lines.js
@@ -1,14 +1,25 @@
+import uuid from 'uuid/v1';
 import actions from '../actions';
 
-const initialState = [
-  { name: 'Red', color: '#fff000' },
-  { name: 'Green', color: '#00c600' },
-  { name: 'Blue', color: '#0273ff' },
-];
-
-export default function(state = initialState, action) {
+export default function(state = {}, action) {
   switch (action.type) {
+    case actions.ADD_LINE:
+      const id = action.line.id || uuid();
+      console.log(action.line);
+      return {...state, [id]: {
+        id,
+        color: action.line.color,
+      }};
+
     default:
       return state;
   }
+}
+
+export function lines(state) {
+  return Object.keys(state).map(id => state[id]);
+}
+
+export function line(state, id) {
+  return state[id];
 }
