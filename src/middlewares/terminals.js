dbcd38d4b71dfd2f091e62e782b3275cc829321a
Henry Catalini Smith <henry@catalinismith.com>
2017-08-24 23:13:54 +0200

    creating the fake connection has been causing the <Terminal /> to get removed from the dom in all the shuffling around that results from creating a connection
    i kind of feel like theres a lesson in here about not doing all these ugly
    little shortcuts across your own data model but idk lol

--- src/middlewares/terminals.js
+++ src/middlewares/terminals.js
@@ -1,13 +1,16 @@
 import uuid from 'uuid/v1';
 import actions from '../actions';
 import { getConnectionsByLine } from '../reducers/connections';
+import { station } from '../reducers/stations';
 
 export default function(store) {
   return next => action => {
     switch (action.type) {
       case actions.ADD_CONNECTION:
-        const { lineId } = action.connection;
-        const { connections } = store.getState();
+        const { lineId, sourceId, destinationId } = action.connection;
+        const { connections, stations } = store.getState();
+        const source = station(stations, sourceId);
+        const destination = station(stations, destinationId);
         const siblings = getConnectionsByLine(connections, lineId)
         if (siblings.length === 0) {
           store.dispatch(actions.addTerminal({
@@ -15,6 +18,8 @@ export default function(store) {
             connectionId: action.connection.id,
             lineId,
             stationId: action.connection.sourceId,
+            x: source.x,
+            y: source.y,
           }));
 
           store.dispatch(actions.addTerminal({
@@ -22,6 +27,8 @@ export default function(store) {
             connectionId: action.connection.id,
             lineId,
             stationId: action.connection.destinationId,
+            x: destination.x,
+            y: destination.y,
           }));
         }
         break;
