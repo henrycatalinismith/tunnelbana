b93a0701bdc6009d776b16c324d4c992d5aedf61
Henry Catalini Smith <smith@hen.cat>
2017-08-24 21:27:54 +0200

    bloody hell this is getting to be a lot of code

--- src/actions/terminals.js
+++ src/actions/terminals.js
@@ -1,8 +1,24 @@
 export const ADD_TERMINAL = 'ADD_TERMINAL';
+export const SELECT_TERMINAL = 'SELECT_TERMINAL';
+export const DESELECT_TERMINAL = 'DESELECT_TERMINAL';
 
 export function addTerminal(terminal) {
   return {
     type: ADD_TERMINAL,
     terminal,
+  };
+}
+
+export function selectTerminal(terminalId) {
+  return {
+    type: SELECT_TERMINAL,
+    terminalId,
+  };
+}
+
+export function deselectTerminal(terminalId) {
+  return {
+    type: DESELECT_TERMINAL,
+    terminalId,
   };
 }
