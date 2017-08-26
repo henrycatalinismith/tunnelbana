a680781a268961037545f6c6bde1747117138408
Henry Catalini Smith <henry@catalinismith.com>
2017-08-26 21:07:34 +0200

    Put terminal x,y coords in the store

--- src/actions/terminals.js
+++ src/actions/terminals.js
@@ -1,6 +1,7 @@
 export const ADD_TERMINAL = 'ADD_TERMINAL';
 export const SELECT_TERMINAL = 'SELECT_TERMINAL';
 export const DESELECT_TERMINAL = 'DESELECT_TERMINAL';
+export const MOVE_TERMINAL = 'MOVE_TERMINAL';
 
 export function addTerminal(terminal) {
   return {
@@ -20,5 +21,14 @@ export function deselectTerminal(selection) {
   return {
     type: DESELECT_TERMINAL,
     ...selection,
+  };
+}
+
+export function moveTerminal(terminalId, x, y) {
+  return {
+    type: MOVE_TERMINAL,
+    terminalId,
+    x,
+    y,
   };
 }
