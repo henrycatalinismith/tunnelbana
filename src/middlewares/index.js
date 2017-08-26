4233f9b4f16eec1e9f86fef74395c5b617e05a18
Henry Smith <henry@henrysmith.org>
2017-08-26 20:24:59 +0200

    Delete pointless middleware

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,11 +1,9 @@
 import terminals from './terminals';
 import trains from './trains';
-import viewBox from './viewBox';
 import window from './window';
 
 export default [
   terminals,
   trains,
-  viewBox,
   window,
 ];
