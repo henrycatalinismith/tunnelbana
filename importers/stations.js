1867511ce5d365eeb0823b653b99e4657ad746a0
Henry Catalini Smith <henry@catalinismith.com>
2017-08-30 08:41:49 +0200

    Okay this is a working station importer

--- importers/stations.js
+++ importers/stations.js
@@ -1,7 +1,27 @@
-import { Svg } from 'svgutils';
+import { Svg, Elements } from 'svgutils';
+import actions from '../actions';
 
 export default function importStations(string, store) {
-  Svg.fromXmlString(string, (error, svg) => {
-    console.log(svg);
+  return new Promise(resolve => {
+    Svg.fromXmlString(string, (error, svg) => {
+      svg.elements
+        .filter(element => element.classes.includes('station'))
+        .forEach(group => importStation(group, store));
+      resolve();
+    });
   });
+}
+
+function importStation(group, store) {
+  const circle = group.childs
+    .filter(child => child instanceof Elements.Circle)
+    .shift();
+
+  const action = actions.importStation({
+    id: group.id,
+    x: circle.cx,
+    y: circle.cy,
+  });
+
+  store.dispatch(action);
 }
