fb6e9e3f520ed1e083b751e8a77bbd1de9262d7f
Henry Catalini Smith <smith@hen.cat>
2017-09-01 09:39:43 +0200

    convert journey reducer to immutable

--- reducers/journeys.js
+++ reducers/journeys.js
@@ -1,31 +1,29 @@
+import Immutable from 'immutable';
 import { createReducer } from 'redux-create-reducer';
 import actions from '../actions';
 
-export default createReducer({}, {
+export default createReducer(new Immutable.Map, {
   [actions.DEPARTURE](state, action) {
-    const id = action.journey.id;
-    return {...state, [id]: {
-      id,
+    return state.set(action.journey.id, Immutable.fromJS({
+      id: action.journey.id,
       sourceId: action.journey.sourceId,
       destinationId: action.journey.destinationId,
       connectionId: action.journey.connectionId,
       lineId: action.journey.lineId,
       trainId: action.journey.trainId,
       isComplete: false,
-    }};
+    }));
   },
 
   [actions.ARRIVAL](state, action) {
-    const id = action.journey.id;
-    let { [id]: {}, ...rest } = state;
-    return rest;
+    return state.delete(action.journey.id);
   }
 });
 
 export function journeys(state) {
-  return Object.keys(state).map(id => state[id]);
+  return state.toList();
 }
 
 export function journey(state, id) {
-  return state[id];
+  return state.get(id);
 }
