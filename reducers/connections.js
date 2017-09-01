96e75bebfe10f7ddfdf8b686a657f40b3783573b
Henry Smith <henry@henrysmith.org>
2017-09-01 08:54:15 +0200

    try immutable

--- reducers/connections.js
+++ reducers/connections.js
@@ -1,17 +1,17 @@
 import { createReducer } from 'redux-create-reducer';
+import Immutable from 'immutable';
 import actions from '../actions';
 
-export default createReducer({}, {
+export default createReducer(new Immutable.Map, {
   [actions.ADD_CONNECTION](state, action) {
-    const id = action.connection.id;
-    return {...state, [id]: {
-      id,
+    return state.set(action.connection.id, Immutable.fromJS({
+      id: action.connection.id,
       sourceId: action.connection.sourceId,
       destinationId: action.connection.destinationId,
       lineId: action.connection.lineId,
       terminalId: undefined,
       tracks: [],
-    }};
+    }));
   },
 
   [actions.ADD_TRACK](state, action) {
@@ -24,71 +24,70 @@ export default createReducer({}, {
       ],
     }};
   }
-
 });
 
 export function connections(state) {
-  return Object.keys(state).map(id => state[id]);
+  return state.toList();
 }
 
 export function connection(state, id) {
-  return state[id];
+  return state.get(id);
 }
 
 export function nextStop(state, previousStationId, currentStationId, lineId) {
-  const isReal = connections(state).filter(c => (
-    !!c.sourceId && !!c.destinationId
+  const isReal = state.filter(c => (
+    !!c.get('sourceId') && !!c.get('destinationId')
   ));
   const goesHere = isReal.filter(c => (
-    c.sourceId === currentStationId
-    || c.destinationId === currentStationId
+    c.get('sourceId') === currentStationId
+    || c.get('destinationId') === currentStationId
   ));
 
   const sameLine = goesHere.filter(c => {
-    return c.lineId == lineId;
+    return c.get('lineId') == lineId;
   });
 
   const onwardsJourney = sameLine.filter(c => {
     const hasNewSource = (
-      c.sourceId !== currentStationId
-      && c.sourceId !== previousStationId
+      c.get('sourceId') !== currentStationId
+      && c.get('sourceId') !== previousStationId
     );
     const hasNewDestination = (
-      c.destinationId !== currentStationId
-      && c.destinationId !== previousStationId
+      c.get('destinationId') !== currentStationId
+      && c.get('destinationId') !== previousStationId
     );
     return hasNewSource || hasNewDestination;
   });
-  if (onwardsJourney.length > 0) {
+  if (onwardsJourney.size > 0) {
     return {
-      connectionId: onwardsJourney[0].id,
-      destinationId: onwardsJourney[0].sourceId !== currentStationId
-        ? onwardsJourney[0].sourceId
-        : onwardsJourney[0].destinationId,
+      connectionId: onwardsJourney.first().get('id'),
+      destinationId: onwardsJourney.first().get('sourceId') !== currentStationId
+        ? onwardsJourney.first().get('sourceId')
+        : onwardsJourney.first().get('destinationId'),
     };
   }
 
   return {
-    connectionId: sameLine[0].id,
-    destinationId: sameLine[0].sourceId !== currentStationId
-      ? sameLine[0].sourceId
-      : sameLine[0].destinationId,
+    connectionId: sameLine.first().get('id'),
+    destinationId: sameLine.first().get('sourceId') !== currentStationId
+      ? sameLine.first().get('sourceId')
+      : sameLine.first().get('destinationId'),
   };
 }
 
 export function getConnectionsByLine(state, lineId) {
-  return connections(state).filter(c => c.lineId === lineId);
+  return connections(state).filter(c => c.get('lineId') === lineId);
 }
 
 export function fakeConnections(state) {
-  return connections(state).filter(c => !!c.terminalId);
+  return connections(state).filter(c => !!c.get('terminalId'));
 }
 
 export function getConnection(state, lineId, sourceId, destinationId) {
   return connections(state).filter(c => (
-    c.lineId === lineId && (
-      (c.sourceId === sourceId && c.destinationId === destinationId)
-      || (c.sourceId === destinationId && c.destinationId === sourceId)
+    c.get('lineId') === lineId && (
+      (c.get('sourceId') === sourceId && c.get('destinationId') === destinationId)
+      || (c.get('sourceId') === destinationId && c.get('destinationId') === sourceId)
     )
   ))[0];
 }
