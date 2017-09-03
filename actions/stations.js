6cc469823eb72c47b5d7204054037b6559b209d9
Henry Catalini Smith <smith@hen.cat>
2017-09-03 22:09:09 +0200

    bit of very broken progress on station moving

--- actions/stations.js
+++ actions/stations.js
@@ -1,5 +1,8 @@
 export const ADD_STATION = "ADD_STATION";
 export const IMPORT_STATION = "IMPORT_STATION";
+export const MOVE_STATION = "MOVE_STATION";
+export const SELECT_STATION = "SELECT_STATION";
+export const DESELECT_STATION = "DESELECT_STATION";
 
 export function addStation(station) {
   return {
@@ -12,5 +15,28 @@ export function importStation(station) {
   return {
     type: IMPORT_STATION,
     station
+  };
+}
+
+export function moveStation(stationId, x, y) {
+  return {
+    type: MOVE_STATION,
+    stationId,
+    x,
+    y
+  };
+}
+
+export function selectStation(id) {
+  return {
+    type: SELECT_STATION,
+    id
+  };
+}
+
+export function deselectStation(selection) {
+  return {
+    type: DESELECT_STATION,
+    id
   };
 }
