c4de9306bfd186390b4c487998f173d2952350c7
Henry Catalini Smith <henry@catalinismith.com>
2017-09-03 17:50:08 +0200

    Make all the reducers completely consistent

--- reducers/map.js
+++ reducers/map.js
@@ -1,7 +1,8 @@
+import Immutable from "immutable";
 import { createReducer } from "redux-create-reducer";
 import actions from "../actions";
 
-const initialState = {
+const initialState = Immutable.fromJS({
   center: {
     x: 0,
     y: 0
@@ -13,17 +14,21 @@ const initialState = {
     width: 0,
     height: 0
   }
-};
+});
 
 export const reducer = createReducer(initialState, {
   [actions.WINDOW_RESIZE](state, action) {
-    return {
-      ...state,
-      viewBox: {
-        ...state.viewBox,
+    return state.update("viewBox", viewBox => {
+      return viewBox.merge({
         width: action.window.width,
         height: action.window.height
-      }
-    };
+      });
+    });
   }
 });
+
+export const selectors = {
+  viewBox(state) {
+    return state.get("viewBox");
+  }
+};
