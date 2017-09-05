b257956e0e968909a7c5b356288af3a291c1de4f
Henry Catalini Smith <smith@hen.cat>
2017-09-05 21:59:54 +0200

    dragon drop working now

--- actions/stations.js
+++ actions/stations.js
@@ -1,8 +1,6 @@
 export const ADD_STATION = "ADD_STATION";
 export const IMPORT_STATION = "IMPORT_STATION";
 export const MOVE_STATION = "MOVE_STATION";
-export const SELECT_STATION = "SELECT_STATION";
-export const DESELECT_STATION = "DESELECT_STATION";
 
 export function addStation(station) {
   return {
@@ -24,19 +22,5 @@ export function moveStation(stationId, x, y) {
     stationId,
     x,
     y
-  };
-}
-
-export function selectStation(id) {
-  return {
-    type: SELECT_STATION,
-    id
-  };
-}
-
-export function deselectStation(id) {
-  return {
-    type: DESELECT_STATION,
-    id
   };
 }
