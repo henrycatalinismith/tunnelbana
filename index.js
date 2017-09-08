0b1da009e6f982a7ad16e41c97cc26a179154c60
Henry Catalini Smith <henry@catalinismith.com>
2017-09-08 18:38:34 +0200

    new track

--- index.js
+++ index.js
@@ -75,8 +75,8 @@ document.addEventListener("DOMContentLoaded", () => {
   } else {
     store.dispatch(
       actions.addLine({
-        id: "Special",
-        color: "Gold"
+        id: "Circle",
+        color: "yellow"
       })
     );
 
@@ -84,6 +84,20 @@ document.addEventListener("DOMContentLoaded", () => {
       actions.addLine({
         id: "Riverside",
         color: "#0273ff"
+      })
+    );
+
+    store.dispatch(
+      actions.addLine({
+        id: "Polar",
+        color: "#ff0000"
+      })
+    );
+
+    store.dispatch(
+      actions.addLine({
+        id: "Hogwarts",
+        color: "#ff00ff"
       })
     );
 
@@ -99,7 +113,7 @@ document.addEventListener("DOMContentLoaded", () => {
       actions.addTrain({
         id: "Gordon",
         stationId: "Kungstradgarden",
-        lineId: "Special"
+        lineId: "Circle"
       })
     );
 
@@ -118,6 +132,13 @@ document.addEventListener("DOMContentLoaded", () => {
       lastId = null,
       blue1 = null,
       blue2 = null;
+
+    const links = [
+      { lineId: "Riverside", from: 0, to: 5 },
+      { lineId: "Polar", from: 2, to: 7 },
+      { lineId: "Hogwarts", from: 4, to: 8 }
+    ];
+
     points(center, 250, 10).map((point, i) => {
       let id = uuid();
       store.dispatch(
@@ -134,7 +155,7 @@ document.addEventListener("DOMContentLoaded", () => {
             id: uuid(),
             sourceId: lastId,
             destinationId: id,
-            lineId: "Special"
+            lineId: "Circle"
           })
         );
       }
@@ -144,7 +165,7 @@ document.addEventListener("DOMContentLoaded", () => {
             id: uuid(),
             sourceId: id,
             destinationId: firstId,
-            lineId: "Special"
+            lineId: "Circle"
           })
         );
       }
@@ -152,32 +173,91 @@ document.addEventListener("DOMContentLoaded", () => {
       if (firstId === null) {
         firstId = id;
       }
+
+      for (let link of links) {
+        if (link.from === i) {
+          link.fromId = id;
+        } else if (link.to === i) {
+          link.toId = id;
+        }
+      }
+
       if (i === 0) blue1 = id;
       if (i === 5) blue2 = id;
       lastId = id;
     });
 
+    for (let link of links) {
+      store.dispatch(
+        actions.addConnection({
+          id: uuid(),
+          sourceId: link.fromId,
+          destinationId: link.toId,
+          lineId: link.lineId
+        })
+      );
+    }
+
     store.dispatch(
-      actions.addConnection({
+      actions.departure({
         id: uuid(),
-        sourceId: blue1,
-        destinationId: blue2,
-        lineId: "Riverside"
+        trainId: "Gordon",
+        sourceId: lastId,
+        destinationId: firstId,
+        lineId: "Circle",
+        connectionId: getConnection(
+          store.getState().get("connections"),
+          "Circle",
+          lastId,
+          firstId
+        ).id
+      })
+    );
+
+    store.dispatch(
+      actions.addTrain({
+        id: "Henry",
+        stationId: links[0].fromId,
+        lineId: "Polar"
+      })
+    );
+
+    store.dispatch(
+      actions.addTrain({
+        id: "Dave",
+        stationId: links[2].fromID,
+        lineId: "Hogwarts"
       })
     );
 
     store.dispatch(
       actions.departure({
         id: uuid(),
-        trainId: "Gordon",
-        sourceId: firstId,
-        destinationId: lastId,
-        lineId: "Special",
+        trainId: "Henry",
+        sourceId: links[1].fromId,
+        destinationId: links[1].toId,
+        lineId: links[1].lineId,
         connectionId: getConnection(
           store.getState().get("connections"),
-          "Special",
-          firstId,
-          lastId
+          links[1].lineId,
+          links[1].fromId,
+          links[1].toId
+        ).id
+      })
+    );
+
+    store.dispatch(
+      actions.departure({
+        id: uuid(),
+        trainId: "Dave",
+        sourceId: links[2].fromId,
+        destinationId: links[2].toId,
+        lineId: links[2].lineId,
+        connectionId: getConnection(
+          store.getState().get("connections"),
+          links[2].lineId,
+          links[2].fromId,
+          links[2].toId
         ).id
       })
     );
