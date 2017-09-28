aa3b4b1a02c19651f906b39134822117169d72b1
Henry Catalini Smith <henry@catalinismith.se>
2017-09-28 19:47:39 +0200

    🐉

--- middlewares/passengers.js
+++ middlewares/passengers.js
@@ -2,40 +2,34 @@ import uuid from "uuid/v1";
 import actions from "../actions";
 import { select } from "../reducers";
 
-export default function(store) {
-  return next => action => {
-    const ret = next(action);
+export function alightTrains(store, action) {
+  const state = store.getState();
+  const trainPassengers = select("passengers")
+    .from(state)
+    .byTrainId(action.journey.trainId);
 
-    switch (action.type) {
-      case actions.ARRIVAL:
-        const state = store.getState();
-        const platformPassengers = select("passengers")
-          .from(state)
-          .byStationId(action.journey.destinationId);
-        const trainPassengers = select("passengers")
-          .from(state)
-          .byTrainId(action.journey.trainId);
+  if (trainPassengers.size > 0) {
+    store.dispatch(
+      actions.alight(
+        trainPassengers.first().get("id"),
+        action.journey.destinationId
+      )
+    );
+  }
+}
 
-        if (platformPassengers.size > 0) {
-          store.dispatch(
-            actions.board(
-              platformPassengers.first().get("id"),
-              action.journey.trainId
-            )
-          );
-        }
+export function boardTrains(store, action) {
+  const state = store.getState();
+  const platformPassengers = select("passengers")
+    .from(state)
+    .byStationId(action.journey.destinationId);
 
-        if (trainPassengers.size > 0) {
-          store.dispatch(
-            actions.alight(
-              trainPassengers.first().get("id"),
-              action.journey.destinationId
-            )
-          );
-        }
-        break;
-    }
-
-    return ret;
-  };
+  if (platformPassengers.size > 0) {
+    store.dispatch(
+      actions.board(
+        platformPassengers.first().get("id"),
+        action.journey.trainId
+      )
+    );
+  }
 }
