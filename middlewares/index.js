659318531d6d55c4b618612c126ac77387889d80
Henry Catalini Smith <henry@catalinismith.com>
2017-09-28 19:47:39 +0200

    🐉

--- middlewares/index.js
+++ middlewares/index.js
@@ -1,7 +1,63 @@
-import dragon from "./dragon";
-import journeys from "./journeys";
-import passengers from "./passengers";
-import terminals from "./terminals";
-import tracks from "./tracks";
+import actions from "../actions";
+import * as dragon from "./dragon";
+import * as journeys from "./journeys";
+import * as passengers from "./passengers";
+import * as terminals from "./terminals";
+import * as tracks from "./tracks";
 
-export default [dragon, journeys, passengers, terminals, tracks];
+const createMiddleware = cb => {
+  const befores = {};
+  const afters = {};
+
+  const middleware = store => {
+    return next => action => {
+      befores[action.type] &&
+        befores[action.type].forEach(before => {
+          before.call(null, store, action);
+        });
+
+      const result = next(action);
+
+      afters[action.type] &&
+        afters[action.type].forEach(after => {
+          after.call(null, store, action);
+        });
+
+      return result;
+    };
+  };
+
+  const addBefore = actionType => {
+    (befores[actionType] = befores[actionType] || []).push(undefined);
+    return `before.${actionType}.${befores[actionType].length}`;
+  };
+  const addAfter = actionType => {
+    (afters[actionType] = afters[actionType] || []).push(undefined);
+    return `after.${actionType}.${afters[actionType].length}`;
+  };
+
+  const middlewares = cb.call(null, addBefore, addAfter);
+
+  Object.keys(middlewares).forEach(key => {
+    const [, timing, actionType, index] = key.match(
+      /^(before|after)\.(.*)+\.(\d+)$/
+    );
+    ({ before: befores, after: afters }[timing][actionType][index - 1] =
+      middlewares[key]);
+  });
+
+  return [middleware];
+};
+
+const middleware = createMiddleware((before, after) => ({
+  [before(actions.DRAGON_MOVE)]: dragon.injectEntity,
+  [after(actions.ADD_CONNECTION)]: tracks.addTracks,
+  [after(actions.ADD_CONNECTION)]: terminals.addTerminal,
+
+  [after(actions.DEPARTURE)]: journeys.animateJourney,
+  [after(actions.ARRIVAL)]: passengers.alightTrains,
+  [after(actions.ARRIVAL)]: passengers.boardTrains,
+  [after(actions.ARRIVAL)]: journeys.scheduleDeparture
+}));
+
+export default middleware;
--- middlewares/terminals.js
+++ middlewares/terminals.js
@@ -2,52 +2,49 @@ import uuid from "uuid/v1";
 import actions from "../actions";
 import { select } from "../reducers";
 
-export default function(store) {
-  return next => action => {
-    switch (action.type) {
-      case actions.ADD_CONNECTION:
-        const { lineId, sourceId, destinationId } = action.connection;
-        const state = store.getState();
-        const connections = state.get("connections");
-        const stations = store.getState().get("stations");
-        const source = select("stations")
-          .from(state)
-          .byId(sourceId)
-          .toJS();
-        const destination = select("stations")
-          .from(state)
-          .byId(destinationId)
-          .toJS();
-        const siblings = select("connections")
-          .from(state)
-          .byLineId(lineId)
-          .toJS();
-        if (siblings.length === 0) {
-          store.dispatch(
-            actions.addTerminal({
-              id: uuid(),
-              connectionId: action.connection.id,
-              lineId,
-              stationId: action.connection.sourceId,
-              x: source.x,
-              y: source.y
-            })
-          );
+export function addTerminal(store, action) {
+  const { lineId, sourceId, destinationId } = action.connection;
 
-          store.dispatch(
-            actions.addTerminal({
-              id: uuid(),
-              connectionId: action.connection.id,
-              lineId,
-              stationId: action.connection.destinationId,
-              x: destination.x,
-              y: destination.y
-            })
-          );
-        }
-        break;
-    }
+  const state = store.getState();
+  const connections = state.get("connections");
+  const stations = store.getState().get("stations");
 
-    return next(action);
-  };
+  const source = select("stations")
+    .from(state)
+    .byId(sourceId)
+    .toJS();
+
+  const destination = select("stations")
+    .from(state)
+    .byId(destinationId)
+    .toJS();
+
+  const siblings = select("connections")
+    .from(state)
+    .byLineId(lineId)
+    .toJS();
+
+  if (siblings.length === 0) {
+    store.dispatch(
+      actions.addTerminal({
+        id: uuid(),
+        connectionId: action.connection.id,
+        lineId,
+        stationId: action.connection.sourceId,
+        x: source.x,
+        y: source.y
+      })
+    );
+
+    store.dispatch(
+      actions.addTerminal({
+        id: uuid(),
+        connectionId: action.connection.id,
+        lineId,
+        stationId: action.connection.destinationId,
+        x: destination.x,
+        y: destination.y
+      })
+    );
+  }
 }
