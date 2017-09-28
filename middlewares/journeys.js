291502fb4c5a37488df8cd4280e8eeb995f1fa56
Henry Smith <henry@henrysmith.org>
2017-09-28 19:47:39 +0200

    🐉

--- middlewares/journeys.js
+++ middlewares/journeys.js
@@ -14,149 +14,142 @@ import { train } from "../reducers/trains";
 import clock from "../clock";
 import * as points from "../geometry/points";
 
-export default function(store) {
-  return next => action => {
-    let state;
-    let train, source, destination, line, journey;
-    const n = next(action);
+export function animateJourney(store, action) {
+  const state = store.getState();
 
-    switch (action.type) {
-      case actions.DEPARTURE:
-        state = store.getState();
-        train = select("trains")
-          .from(state)
-          .byId(action.journey.trainId)
-          .toJS();
-        source = select("stations")
-          .from(state)
-          .byId(action.journey.sourceId)
-          .toJS();
-        destination = select("stations")
-          .from(state)
-          .byId(action.journey.destinationId)
-          .toJS();
-        line = select("lines")
-          .from(state)
-          .byId(action.journey.lineId)
-          .toJS();
-        journey = select("journeys")
-          .from(state)
-          .byId(train.journeyId)
-          .toJS();
-        const tracks = select("tracks")
-          .from(state)
-          .forJourney(action.journey)
-          .toJS();
+  const train = select("trains")
+    .from(state)
+    .byId(action.journey.trainId)
+    .toJS();
 
-        const width = 15;
-        const height = 30;
-        //const halfTrain = { x: 0 - height / 2, y: 0 - width / 2 };
-        const halfTrain = { x: 0, y: 0 };
-        let from = points.add(source, halfTrain);
-        let to = points.add(destination, halfTrain);
+  const source = select("stations")
+    .from(state)
+    .byId(action.journey.sourceId)
+    .toJS();
 
-        const speed = 0.1;
-        const distance = points.distance(from, to);
-        const time = distance / speed;
-        let angle = points.angle(to, from);
+  const destination = select("stations")
+    .from(state)
+    .byId(action.journey.destinationId)
+    .toJS();
 
-        const pathId = `#connection-${source.id}-${destination.id}-${line.id}`;
-        const path = document.querySelector(pathId);
-        let degrees = angle * 180 / Math.PI;
+  const line = select("lines")
+    .from(state)
+    .byId(action.journey.lineId)
+    .toJS();
 
-        if (tracks.length === 1) {
-          //TweenLite.to(`#${train.id}`, 0.1, {
-          //rotation: degrees,
-          //svgOrigin: `${source.x} ${source.y}`
-          //});
-          TweenLite.fromTo(`#${train.id}`, time / 1000, from, to);
-        } else {
-          let prevAngle = 0;
-          let turnAngle;
-          const tweens = [];
-          let i = 0;
+  const journey = select("journeys")
+    .from(state)
+    .byId(train.journeyId)
+    .toJS();
+  const tracks = select("tracks")
+    .from(state)
+    .forJourney(action.journey)
+    .toJS();
 
-          for (let track of tracks) {
-            from = points.add({ x: track.x1, y: track.y1 }, halfTrain);
-            to = points.add({ x: track.x2, y: track.y2 }, halfTrain);
+  const width = 15;
+  const height = 30;
+  //const halfTrain = { x: 0 - height / 2, y: 0 - width / 2 };
+  const halfTrain = { x: 0, y: 0 };
+  let from = points.add(source, halfTrain);
+  let to = points.add(destination, halfTrain);
 
-            angle = points.angle(from, to);
-            degrees = angle * 180 / Math.PI;
-            turnAngle = prevAngle - degrees;
+  const speed = 0.1;
+  const distance = points.distance(from, to);
+  const time = distance / speed;
+  let angle = points.angle(to, from);
 
-            if (turnAngle < 0) {
-              turnAngle += 360;
-            }
-            if (degrees < 0) {
-              degrees += 360;
-            }
-            // this is literally a special magic property of trains that they
-            // can go forwards or backwards and it's just fine
-            if (degrees === 180) {
-              degrees = 0;
-            }
-            const needsTurn = turnAngle !== 180;
+  const pathId = `#connection-${source.id}-${destination.id}-${line.id}`;
+  const path = document.querySelector(pathId);
+  let degrees = angle * 180 / Math.PI;
 
-            //tl.to(`#${train.id}`, 0.001, points.add(from, halfTrain));
-            //tweens.push(
-            //TweenMax.to(`#${train.id}`, 0.01, {
-            //rotation: degrees,
-            //delay: time / 1000 / 3 * i
-            //})
-            //);
-            tweens.push(
-              TweenMax.fromTo(`#${train.id}`, time / 1000 / 3, from, {
-                ...to,
-                ease: Linear.easeNone,
-                delay: time / 1000 / 3 * i
-              })
-            );
-            prevAngle = degrees;
-            i++;
-          }
+  if (tracks.length === 1) {
+    //TweenLite.to(`#${train.id}`, 0.1, {
+    //rotation: degrees,
+    //svgOrigin: `${source.x} ${source.y}`
+    //});
+    TweenLite.fromTo(`#${train.id}`, time / 1000, from, to);
+  } else {
+    let prevAngle = 0;
+    let turnAngle;
+    const tweens = [];
+    let i = 0;
 
-          const tl = new TimelineMax({ tweens });
-          tl.play();
-        }
+    for (let track of tracks) {
+      from = points.add({ x: track.x1, y: track.y1 }, halfTrain);
+      to = points.add({ x: track.x2, y: track.y2 }, halfTrain);
 
-        clock.setTimeout(() => {
-          store.dispatch(
-            actions.arrival({
-              id: journey.id,
-              destinationId: destination.id,
-              lineId: line.id,
-              sourceId: source.id,
-              trainId: train.id
-            })
-          );
-        }, time);
-        break;
+      angle = points.angle(from, to);
+      degrees = angle * 180 / Math.PI;
+      turnAngle = prevAngle - degrees;
 
-      case actions.ARRIVAL:
-        state = store.getState();
-        const { connectionId, destinationId } = select("connections")
-          .from(state)
-          .forNextStop(
-            action.journey.sourceId,
-            action.journey.destinationId,
-            action.journey.lineId
-          );
+      if (turnAngle < 0) {
+        turnAngle += 360;
+      }
+      if (degrees < 0) {
+        degrees += 360;
+      }
+      // this is literally a special magic property of trains that they
+      // can go forwards or backwards and it's just fine
+      if (degrees === 180) {
+        degrees = 0;
+      }
+      const needsTurn = turnAngle !== 180;
 
-        clock.setTimeout(() => {
-          store.dispatch(
-            actions.departure({
-              id: uuid(),
-              sourceId: action.journey.destinationId,
-              destinationId: destinationId,
-              connectionId: connectionId,
-              trainId: action.journey.trainId,
-              lineId: action.journey.lineId
-            })
-          );
-        }, 1000);
-        break;
+      //tl.to(`#${train.id}`, 0.001, points.add(from, halfTrain));
+      //tweens.push(
+      //TweenMax.to(`#${train.id}`, 0.01, {
+      //rotation: degrees,
+      //delay: time / 1000 / 3 * i
+      //})
+      //);
+      tweens.push(
+        TweenMax.fromTo(`#${train.id}`, time / 1000 / 3, from, {
+          ...to,
+          ease: Linear.easeNone,
+          delay: time / 1000 / 3 * i
+        })
+      );
+      prevAngle = degrees;
+      i++;
     }
 
-    return n;
-  };
+    const tl = new TimelineMax({ tweens });
+    tl.play();
+  }
+
+  clock.setTimeout(() => {
+    store.dispatch(
+      actions.arrival({
+        id: journey.id,
+        destinationId: destination.id,
+        lineId: line.id,
+        sourceId: source.id,
+        trainId: train.id
+      })
+    );
+  }, time);
+}
+
+export function scheduleDeparture(store, action) {
+  const state = store.getState();
+  const { connectionId, destinationId } = select("connections")
+    .from(state)
+    .forNextStop(
+      action.journey.sourceId,
+      action.journey.destinationId,
+      action.journey.lineId
+    );
+
+  clock.setTimeout(() => {
+    store.dispatch(
+      actions.departure({
+        id: uuid(),
+        sourceId: action.journey.destinationId,
+        destinationId: destinationId,
+        connectionId: connectionId,
+        trainId: action.journey.trainId,
+        lineId: action.journey.lineId
+      })
+    );
+  }, 1000);
 }
--- middlewares/tracks.js
+++ middlewares/tracks.js
@@ -3,138 +3,121 @@ import actions from "../actions";
 import { select } from "../reducers";
 import * as points from "../geometry/points";
 
-export default function(store) {
-  return next => action => {
-    const ret = next(action);
+export function addTracks(store, action) {
+  const { lineId, sourceId, destinationId } = action.connection;
+  const state = store.getState();
+  const connections = store.getState().get("connections");
+  const stations = store.getState().get("stations");
 
-    switch (action.type) {
-      case actions.DRAGON_MOVE:
-        if (action.entity !== "station") break;
-        let con = select("connections")
-          .from(store.getState())
-          .byStationId(action.id)
-          .first()
-          .toJS();
-        action.connection = con;
+  const source = select("stations")
+    .from(state)
+    .byId(sourceId)
+    .toJS();
 
-      case actions.ADD_CONNECTION:
-        const { lineId, sourceId, destinationId } = action.connection;
-        const state = store.getState();
-        const connections = store.getState().get("connections");
-        const stations = store.getState().get("stations");
-        const source = select("stations")
-          .from(state)
-          .byId(sourceId)
-          .toJS();
-        const destination = select("stations")
-          .from(state)
-          .byId(destinationId)
-          .toJS();
-        const angle = Math.abs(points.angle(source, destination));
-        const π = Math.PI;
+  const destination = select("stations")
+    .from(state)
+    .byId(destinationId)
+    .toJS();
 
-        let type = "dunno lol";
+  const angle = Math.abs(points.angle(source, destination));
+  const π = Math.PI;
 
-        if (angle > π / 4 && angle < 3 / 4 * π) {
-          type = "latitudinal"; // northerly track
-        } else if (angle > 3 / 4 * π && angle < 5 / 4 * π) {
-          type = "longitudinal"; // westerly track
-        } else if (angle > 5 / 4 * π && angle < 7 / 4 * π) {
-          type = "latitudinal"; // southerly track
-        } else {
-          type = "longitudinal"; // easterly track
-        }
+  let type = "dunno lol";
 
-        let primaryDistance;
-        let secondaryDistance;
-        if (type === "latitudinal") {
-          primaryDistance = source.y - destination.y;
-          secondaryDistance = source.x - destination.x;
-        } else {
-          primaryDistance = source.x - destination.x;
-          secondaryDistance = source.y - destination.y;
-        }
-        primaryDistance = Math.abs(primaryDistance);
-        secondaryDistance = Math.abs(secondaryDistance);
-        const remainder = primaryDistance - secondaryDistance;
+  if (angle > π / 4 && angle < 3 / 4 * π) {
+    type = "latitudinal"; // northerly track
+  } else if (angle > 3 / 4 * π && angle < 5 / 4 * π) {
+    type = "longitudinal"; // westerly track
+  } else if (angle > 5 / 4 * π && angle < 7 / 4 * π) {
+    type = "latitudinal"; // southerly track
+  } else {
+    type = "longitudinal"; // easterly track
+  }
 
-        if (Math.abs(secondaryDistance) < 0.000001) {
-          store.dispatch(
-            actions.addTrack({
-              id: uuid(),
-              connectionId: action.connection.id,
-              lineId,
-              sourceId: action.connection.sourceId,
-              destinationId: action.connection.destinationId,
-              ordinality: 0,
-              x1: source.x,
-              y1: source.y,
-              x2: destination.x,
-              y2: destination.y
-            })
-          );
+  let primaryDistance;
+  let secondaryDistance;
+  if (type === "latitudinal") {
+    primaryDistance = source.y - destination.y;
+    secondaryDistance = source.x - destination.x;
+  } else {
+    primaryDistance = source.x - destination.x;
+    secondaryDistance = source.y - destination.y;
+  }
+  primaryDistance = Math.abs(primaryDistance);
+  secondaryDistance = Math.abs(secondaryDistance);
+  const remainder = primaryDistance - secondaryDistance;
 
-          store.dispatch(
-            actions.addTrack({
-              id: uuid(),
-              connectionId: action.connection.id,
-              lineId,
-              sourceId: action.connection.destinationId,
-              destinationId: action.connection.sourceId,
-              ordinality: 0,
-              x1: destination.x,
-              y1: destination.y,
-              x2: source.x,
-              y2: source.y
-            })
-          );
-        } else {
-          let p1, p2;
+  if (Math.abs(secondaryDistance) < 0.000001) {
+    store.dispatch(
+      actions.addTrack({
+        id: uuid(),
+        connectionId: action.connection.id,
+        lineId,
+        sourceId: action.connection.sourceId,
+        destinationId: action.connection.destinationId,
+        ordinality: 0,
+        x1: source.x,
+        y1: source.y,
+        x2: destination.x,
+        y2: destination.y
+      })
+    );
 
-          const addTrack = (p1, p2, sourceId, destinationId, ordinality) => {
-            store.dispatch(
-              actions.addTrack({
-                id: uuid(),
-                connectionId: action.connection.id,
-                lineId,
-                sourceId,
-                destinationId,
-                ordinality,
-                x1: p1.x,
-                y1: p1.y,
-                x2: p2.x,
-                y2: p2.y
-              })
-            );
-          };
+    store.dispatch(
+      actions.addTrack({
+        id: uuid(),
+        connectionId: action.connection.id,
+        lineId,
+        sourceId: action.connection.destinationId,
+        destinationId: action.connection.sourceId,
+        ordinality: 0,
+        x1: destination.x,
+        y1: destination.y,
+        x2: source.x,
+        y2: source.y
+      })
+    );
+  } else {
+    let p1, p2;
 
-          let a, b;
-          if (type === "latitudinal") {
-            a = { x: source.x, y: source.y };
-            b = { x: destination.x, y: destination.y };
-            a.y += source.y < destination.y ? remainder / 2 : 0 - remainder / 2;
-            b.y += source.y > destination.y ? remainder / 2 : 0 - remainder / 2;
+    const addTrack = (p1, p2, sourceId, destinationId, ordinality) => {
+      store.dispatch(
+        actions.addTrack({
+          id: uuid(),
+          connectionId: action.connection.id,
+          lineId,
+          sourceId,
+          destinationId,
+          ordinality,
+          x1: p1.x,
+          y1: p1.y,
+          x2: p2.x,
+          y2: p2.y
+        })
+      );
+    };
 
-            addTrack(source, a, source.id, destination.id, 0);
-            addTrack(a, b, source.id, destination.id, 1);
-            addTrack(b, destination, source.id, destination.id, 2);
-          }
+    let a, b;
+    if (type === "latitudinal") {
+      a = { x: source.x, y: source.y };
+      b = { x: destination.x, y: destination.y };
+      a.y += source.y < destination.y ? remainder / 2 : 0 - remainder / 2;
+      b.y += source.y > destination.y ? remainder / 2 : 0 - remainder / 2;
 
-          if (type === "longitudinal") {
-            a = { x: source.x, y: source.y };
-            b = { x: destination.x, y: destination.y };
-            a.x += source.x < destination.x ? remainder / 2 : 0 - remainder / 2;
-            b.x += source.x > destination.x ? remainder / 2 : 0 - remainder / 2;
-
-            addTrack(source, a, source.id, destination.id, 0);
-            addTrack(a, b, source.id, destination.id, 1);
-            addTrack(b, destination, source.id, destination.id, 2);
-          }
-        }
-
-        break;
+      addTrack(source, a, source.id, destination.id, 0);
+      addTrack(a, b, source.id, destination.id, 1);
+      addTrack(b, destination, source.id, destination.id, 2);
     }
 
-    return ret;
-  };
+    if (type === "longitudinal") {
+      a = { x: source.x, y: source.y };
+      b = { x: destination.x, y: destination.y };
+      a.x += source.x < destination.x ? remainder / 2 : 0 - remainder / 2;
+      b.x += source.x > destination.x ? remainder / 2 : 0 - remainder / 2;
+
+      addTrack(source, a, source.id, destination.id, 0);
+      addTrack(a, b, source.id, destination.id, 1);
+      addTrack(b, destination, source.id, destination.id, 2);
+    }
+  }
 }
