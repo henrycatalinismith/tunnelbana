f1e3bdbe635049402b350a1c9b072ead60db4844
Henry Catalini Smith <smith@hen.cat>
2017-09-28 19:47:39 +0200

    🐉

--- middlewares/dragon.js
+++ middlewares/dragon.js
@@ -2,20 +2,12 @@ import uuid from "uuid/v1";
 import actions from "../actions";
 import { select } from "../reducers";
 
-export default function(store) {
-  return next => action => {
-    switch (action.type) {
-      case actions.DRAGON_MOVE:
-        const dragon = store.getState().get("dragon");
-        const entity = dragon.get("entity");
-        const id = dragon.get("id");
-        if (entity && id) {
-          action.entity = entity;
-          action.id = id;
-        }
-        break;
-    }
-
-    return next(action);
-  };
+export function injectEntity(store, action) {
+  const dragon = store.getState().get("dragon");
+  const entity = dragon.get("entity");
+  const id = dragon.get("id");
+  if (entity && id) {
+    action.entity = entity;
+    action.id = id;
+  }
 }
