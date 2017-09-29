5fa96fea90013b5e755871e1ddcd2c9237c85207
Henry Catalini Smith <henry@catalinismith.se>
2017-09-29 19:38:16 +0200

    Add cancel

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 exports.createMiddleware = cb => {
   const befores = {};
   const afters = {};
+  const cancels = {};
 
   const middleware = store => {
     return next => action => {
@@ -8,6 +9,15 @@ exports.createMiddleware = cb => {
         befores[action.type].forEach(before => {
           before.call(null, store, action);
         });
+
+      if (cancels[action.type]) {
+        for (let cancel of cancels[action.type]) {
+          const shouldCancel = cancel.call(null, store, action);
+          if (shouldCancel === true) {
+            return null;
+          }
+        }
+      }
 
       const result = next(action);
 
@@ -24,18 +34,24 @@ exports.createMiddleware = cb => {
     (befores[actionType] = befores[actionType] || []).push(undefined);
     return `before.${actionType}.${befores[actionType].length}`;
   };
+
   const addAfter = actionType => {
     (afters[actionType] = afters[actionType] || []).push(undefined);
     return `after.${actionType}.${afters[actionType].length}`;
   };
 
-  const middlewares = cb.call(null, addBefore, addAfter);
+  const addCancel = actionType => {
+    (cancels[actionType] = cancels[actionType] || []).push(undefined);
+    return `cancel.${actionType}.${cancels[actionType].length}`;
+  };
+
+  const middlewares = cb.call(null, addBefore, addAfter, addCancel);
 
   Object.keys(middlewares).forEach(key => {
     const [, timing, actionType, index] = key.match(
-      /^(before|after)\.(.*)+\.(\d+)$/
+      /^(before|after|cancel)\.(.*)+\.(\d+)$/
     );
-    ({ before: befores, after: afters }[timing][actionType][index - 1] =
+    ({ before: befores, after: afters, cancel: cancels }[timing][actionType][index - 1] =
       middlewares[key]);
   });
 
