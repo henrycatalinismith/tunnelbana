fa9203e2350e0b0ef9b847dc029c2f56f27ded10
Henry Catalini Smith <smith@hen.cat>
2017-10-01 18:41:10 +0200

    finish up

---
+++ middlewares/stations.js
@@ -0,0 +1,11 @@
+import { createMiddleware } from "signalbox";
+import uuid from "uuid/v1";
+import actions from "../actions";
+
+export const middleware = createMiddleware((before, after, cancel) => ({
+  [before(actions.ADD_STATION)](store, action) {
+    if (!action.station.id) {
+      action.station.id = uuid();
+    }
+  }
+}));
