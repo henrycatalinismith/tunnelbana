3df8e6a7b8ccb7f25544f05945be833369885182
Henry Smith <henry@henrysmith.org>
2017-10-01 18:41:10 +0200

    finish up

--- middlewares/index.js
+++ middlewares/index.js
@@ -3,15 +3,21 @@ import actions from "../actions";
 import { middleware as connections } from "./connections";
 import { middleware as dragon } from "./dragon";
 import { middleware as journeys } from "./journeys";
+import { middleware as lines } from "./lines";
 import { middleware as passengers } from "./passengers";
+import { middleware as stations } from "./stations";
 import { middleware as terminals } from "./terminals";
 import { middleware as tracks } from "./tracks";
+import { middleware as trains } from "./trains";
 
 export default [
   ...connections,
   ...dragon,
   ...journeys,
+  ...lines,
+  ...stations,
   ...terminals,
   ...tracks,
+  ...trains,
   ...passengers
 ];
