10e7f0b7402f418c78bc192c151a12b3de7efa43
Henry Smith <henry@henrysmith.org>
2017-10-02 18:44:08 +0200

    Sort of merge in the other thing

--- index.js
+++ index.js
@@ -58,3 +58,42 @@ exports.createMiddleware = cb => {
   return [middleware];
 };
 
+
+const defaultOptions = {
+  stateAccessor(state, entity) {
+    return state && state[entity];
+  }
+};
+
+exports.createSelect = (selectors, options = defaultOptions) => {
+  const select = entity => {
+    return {
+      from(state) {
+        return new Proxy({}, {
+          get(target, name) {
+            const stateParam = options.stateAccessor(state, entity);
+            return selectors[entity][name].bind(null, stateParam);
+          }
+        });
+      }
+    }
+  };
+
+  select.bindStore = store => {
+    const boundSelect = {};
+
+    for (let entity in selectors) {
+      boundSelect[entity] = {};
+      for (let name in selectors[entity]) {
+        boundSelect[entity][name] = () => {
+          const state = store.getState();
+          const stateParam = options.stateAccessor(state, entity);
+          return selectors[entity][name].call(null, stateParam);
+        };
+      }
+    }
+    return boundSelect;
+  }
+
+  return select;
+}
