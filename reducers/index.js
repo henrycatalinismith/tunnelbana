bb9eadcb60ff95bcbf631980fd9d2e68ac0c6bba
Henry Catalini Smith <henry@catalinismith.se>
2017-10-02 19:54:34 +0200

    bloody hell it did work

--- reducers/index.js
+++ reducers/index.js
@@ -81,7 +81,7 @@ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   reducer,
   undefined,
-  composeEnhancers(applyMiddleware(...middlewares))
+  composeEnhancers(applyMiddleware(middlewares))
 );
 
 export default store;
