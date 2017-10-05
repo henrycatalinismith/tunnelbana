b8626b2389c1d3d6b964bad939a3f975d41e9f25
Henry Catalini Smith <henry@catalinismith.se>
2017-10-05 18:49:27 +0200

    whooooop

--- whiteboards/henry.js
+++ whiteboards/henry.js
@@ -1,60 +1,147 @@
 import { start } from "../index";
+import uuid from "uuid/v1";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const app = document.createElement("div");
-  document.body.appendChild(app);
+  const engineRoot = document.createElement("div");
+  document.body.appendChild(engineRoot);
+  const options = { element: engineRoot };
 
-  const options = { element: app };
   start(options, game => {
+    game.dispatch.addLine({
+      id: "Circle",
+      color: "yellow"
+    });
+
     game.dispatch.addLine({
       id: "Riverside",
       color: "#0273ff"
     });
 
-    game.dispatch.addStation({
-      id: "one",
-      x: 100,
-      y: 100
+    game.dispatch.addLine({
+      id: "Polar",
+      color: "#ff0000"
     });
 
-    game.dispatch.addStation({
-      id: "two",
-      x: 200,
-      y: 100
+    game.dispatch.addLine({
+      id: "Hogwarts",
+      color: "#ff00ff"
+    });
+    const π = Math.PI;
+    const points = ({ x, y }, r, n) => {
+      const angle = 2 * π / n;
+
+      return [...Array(n)].map((_, i) => ({
+        x: x + r * Math.cos(angle * i),
+        y: y + r * Math.sin(angle * i)
+      }));
+    };
+
+    const center = { x: 300, y: 300 };
+    let firstId = null,
+      lastId = null,
+      blue1 = null,
+      blue2 = null;
+
+    const links = [
+      { lineId: "Riverside", from: 0, to: 5 },
+      { lineId: "Polar", from: 2, to: 7 },
+      { lineId: "Hogwarts", from: 4, to: 8 }
+    ];
+
+    points(center, 250, 10).map((point, i) => {
+      let id = uuid();
+      game.dispatch.addStation({
+        id: id,
+        x: point.x,
+        y: point.y
+      });
+
+      if (lastId) {
+        game.dispatch.addConnection({
+          sourceId: lastId,
+          destinationId: id,
+          lineId: "Circle"
+        });
+      }
+      if (i === 9) {
+        game.dispatch.addConnection({
+          sourceId: id,
+          destinationId: firstId,
+          lineId: "Circle"
+        });
+      }
+
+      if (firstId === null) {
+        firstId = id;
+      }
+
+      for (let link of links) {
+        if (link.from === i) {
+          link.fromId = id;
+        } else if (link.to === i) {
+          link.toId = id;
+        }
+      }
+
+      if (i === 0) blue1 = id;
+      if (i === 5) blue2 = id;
+      lastId = id;
     });
 
-    game.dispatch.addStation({
-      id: "three",
-      x: 150,
-      y: 500
+    for (let link of links) {
+      game.dispatch.addConnection({
+        sourceId: link.fromId,
+        destinationId: link.toId,
+        lineId: link.lineId
+      });
+    }
+
+    game.dispatch.addTrain({
+      id: "Henry",
+      stationId: links[1].fromId,
+      lineId: links[1].lineId
     });
 
-    //game.dispatch.addConnection({
-    //sourceId: "one",
-    //destinationId: "two",
-    //lineId: "Riverside"
-    //});
+    game.dispatch.addTrain({
+      id: "Dave",
+      stationId: links[2].fromId,
+      lineId: links[2].lineId
+    });
 
-    //game.dispatch.addConnection({
-    //sourceId: "two",
-    //destinationId: "three",
-    //lineId: "Riverside"
-    //});
+    game.dispatch.addTrain({
+      id: "Gordon",
+      stationId: lastId,
+      lineId: "Circle"
+    });
 
-    //game.dispatch.addTrain({
-    //id: "Henry",
-    //stationId: "one",
-    //lineId: "Riverside"
-    //});
+    game.dispatch.addTrain({
+      id: "Thomas",
+      stationId: blue1,
+      lineId: "Riverside"
+    });
 
-    //game.dispatch.departure({
-    //trainId: "Henry",
-    //destinationId: "two"
-    //});
+    game.dispatch.departure({
+      trainId: "Henry",
+      destinationId: links[1].toId
+    });
 
-    //game.dispatch.windowResize({
-    //width: 300,
-    //height: 300
-    //});
+    game.dispatch.departure({
+      trainId: "Dave",
+      destinationId: links[2].toId
+    });
+
+    game.dispatch.departure({
+      trainId: "Gordon",
+      destinationId: firstId
+    });
+
+    game.dispatch.departure({
+      trainId: "Thomas",
+      destinationId: blue2
+    });
+
+    game.dispatch.addPassenger({
+      stationId: blue1
+    });
   });
 });
