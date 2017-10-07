b905eff030c0f1b4b7d4cd5850204a6c7e34fb0f
Henry Catalini Smith <smith@hen.cat>
2017-10-07 18:33:52 +0200

    Add updateConnection

--- actions/connections.js
+++ actions/connections.js
@@ -1,8 +1,16 @@
 export const CREATE_CONNECTION = "CREATE_CONNECTION";
+export const UPDATE_CONNECTION = "UPDATE_CONNECTION";
 
 export function createConnection(connection) {
   return {
     type: CREATE_CONNECTION,
+    connection
+  };
+}
+
+export function updateConnection(connection) {
+  return {
+    type: UPDATE_CONNECTION,
     connection
   };
 }
