6069d6a8a7857051dc9012683bf298e736a4c9f8
Henry Catalini Smith <smith@hen.cat>
2017-10-08 10:58:34 +0200

    Make a plan

--- actions/dragon.js
+++ actions/dragon.js
@@ -1,21 +1,37 @@
+// keep
+export const DRAGON_DROP = "DRAGON_DROP";
 export const DRAGON_GRAB = "DRAGON_GRAB";
-export const DRAGON_GRAB_STATION = "DRAGON_GRAB_STATION";
-export const DRAGON_GRAB_TERMINAL = "DRAGON_GRAB_TERMINAL";
-
 export const DRAGON_MOVE = "DRAGON_MOVE";
+
+// deprecate
 export const DRAGON_MOVE_STATION = "DRAGON_MOVE_STATION";
 export const DRAGON_MOVE_TERMINAL = "DRAGON_MOVE_TERMINAL";
-
-export const DRAGON_DROP = "DRAGON_DROP";
+export const DRAGON_GRAB_STATION = "DRAGON_GRAB_STATION";
+export const DRAGON_GRAB_TERMINAL = "DRAGON_GRAB_TERMINAL";
 export const DRAGON_DROP_TERMINAL = "DRAGON_DROP_TERMINAL";
-
 export const DRAGON_CREATE_CONNECTION = "DRAGON_CREATE_CONNECTION";
+
+export function dragonDrop(entity, id) {
+  return {
+    type: DRAGON_DROP,
+    entity,
+    id
+  };
+}
 
 export function dragonGrab(entity, id) {
   return {
     type: DRAGON_GRAB,
     entity,
     id
+  };
+}
+
+export function dragonMove(x, y) {
+  return {
+    type: DRAGON_MOVE,
+    x,
+    y
   };
 }
 
@@ -33,14 +49,6 @@ export function dragonGrabTerminal(id) {
   };
 }
 
-export function dragonMove(x, y) {
-  return {
-    type: DRAGON_MOVE,
-    x,
-    y
-  };
-}
-
 export function dragonMoveStation(x, y, id) {
   return {
     type: DRAGON_MOVE_STATION,
@@ -55,14 +63,6 @@ export function dragonMoveTerminal(x, y, id) {
     type: DRAGON_MOVE_TERMINAL,
     x,
     y,
-    id
-  };
-}
-
-export function dragonDrop(entity, id) {
-  return {
-    type: DRAGON_DROP,
-    entity,
     id
   };
 }
