3c628ad1b0e82f4afe0aad0213df07acce7fc2fe
Henry Smith <henry@henrysmith.org>
2017-10-09 21:04:09 +0200

    fix it

--- actions/stations.js
+++ actions/stations.js
@@ -1,6 +1,10 @@
 export const CREATE_STATION = "CREATE_STATION";
 export const IMPORT_STATION = "IMPORT_STATION";
 
+export const DRAG_STATION = "DRAG_STATION";
+export const DROP_STATION = "DROP_STATION";
+export const GRAB_STATION = "GRAB_STATION";
+
 export function createStation(station) {
   return {
     type: CREATE_STATION,
@@ -12,5 +16,12 @@ export function importStation(station) {
   return {
     type: IMPORT_STATION,
     station
+  };
+}
+
+export function grabStation(id) {
+  return {
+    type: GRAB_STATION,
+    station: { id }
   };
 }
