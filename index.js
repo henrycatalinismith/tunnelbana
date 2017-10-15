346e6d99efd35e5ab9aacc2ff0edb132085dab3a
Henry Catalini Smith <henry@catalinismith.se>
2017-10-15 19:43:08 +0200

    Add createReducer

--- index.js
+++ index.js
@@ -117,6 +117,20 @@ const defaultOptions = {
   }
 };
 
+exports.createReducer = (initialState, actionHandlers) => {
+  const reducer = (state, action) => {
+    if (state === undefined) {
+      state = initialState;
+    }
+    if (actionHandlers.hasOwnProperty(action.type)) {
+      state = actionHandlers[action.type](state, action);
+    }
+    return state;
+  };
+
+  return reducer;
+};
+
 exports.createSelect = (selectors, options = defaultOptions) => {
   const select = entity => {
     return {
