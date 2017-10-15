e7b0a49d3f3c585f0ff9453f223bce900d72472b
Henry Catalini Smith <henry@catalinismith.com>
2017-10-15 19:45:24 +0200

    switch out createreducer

--- reducers/dragon.js
+++ reducers/dragon.js
@@ -1,69 +1,70 @@
 import Immutable from "immutable";
-import { createReducer } from "redux-create-reducer";
+import { createReducer } from "signalbox";
 import actions from "../actions";
 
-const initialState = Immutable.fromJS({
-  entity: undefined,
-  id: undefined,
-  x: 0,
-  y: 0
-});
+export const reducer = createReducer(
+  Immutable.fromJS({
+    entity: undefined,
+    id: undefined,
+    x: 0,
+    y: 0
+  }),
+  {
+    [actions.DRAGON_GRAB](state, action) {
+      return state.merge(
+        Immutable.fromJS({
+          entity: action.dragon.entity,
+          id: action.dragon.id
+        })
+      );
+    },
 
-export const reducer = createReducer(initialState, {
-  [actions.DRAGON_GRAB](state, action) {
-    return state.merge(
-      Immutable.fromJS({
-        entity: action.dragon.entity,
-        id: action.dragon.id
-      })
-    );
-  },
+    [actions.IMAGINE_CONNECTION](state, action) {
+      return state.merge(
+        Immutable.fromJS({
+          entity: "terminal",
+          id: action.destinationTerminal.id
+        })
+      );
+    },
 
-  [actions.IMAGINE_CONNECTION](state, action) {
-    return state.merge(
-      Immutable.fromJS({
-        entity: "terminal",
-        id: action.destinationTerminal.id
-      })
-    );
-  },
+    [actions.ABANDON_CONNECTION](state, action) {
+      return state.merge(
+        Immutable.fromJS({
+          entity: undefined,
+          id: undefined
+        })
+      );
+    },
 
-  [actions.ABANDON_CONNECTION](state, action) {
-    return state.merge(
-      Immutable.fromJS({
-        entity: undefined,
-        id: undefined
-      })
-    );
-  },
+    [actions.DRAGON_MOVE](state, action) {
+      return state.merge(
+        Immutable.fromJS({
+          x: action.dragon.x,
+          y: action.dragon.y
+        })
+      );
+    },
 
-  [actions.DRAGON_MOVE](state, action) {
-    return state.merge(
-      Immutable.fromJS({
-        x: action.dragon.x,
-        y: action.dragon.y
-      })
-    );
-  },
+    [actions.DRAG_TERMINAL](state, action) {
+      return state.merge(
+        Immutable.fromJS({
+          x: action.terminal.x,
+          y: action.terminal.y
+        })
+      );
+    },
 
-  [actions.DRAG_TERMINAL](state, action) {
-    return state.merge(
-      Immutable.fromJS({
-        x: action.terminal.x,
-        y: action.terminal.y
-      })
-    );
-  },
-
-  [actions.DRAGON_DROP](state, action) {
-    return state.merge(
-      Immutable.fromJS({
-        entity: undefined,
-        id: undefined
-      })
-    );
+    [actions.DRAGON_DROP](state, action) {
+      return state.merge(
+        Immutable.fromJS({
+          entity: undefined,
+          id: undefined
+        })
+      );
+    }
   }
-});
+);
 
 export const selectors = {
   all(state) {
