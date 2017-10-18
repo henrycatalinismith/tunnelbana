a0a54d497062c90d35976b4d9cf78f9b82a24e17
Henry Catalini Smith <henry@catalinismith.se>
2017-10-18 17:49:25 +0200

    add app.select test

--- src/__tests__/createApp.js
+++ src/__tests__/createApp.js
@@ -1,7 +1,11 @@
 describe("createApp", () => {
   let createApp;
   let mockStore = { dispatch: jest.fn() };
-  let mockSelectors = { bindStore: jest.fn() };
+  let mockSelectors = {
+    bindStore: jest.fn((selectors, store) => {
+      return Object.assign({}, selectors, { bound: true } );
+    })
+  };
   let mockBindActionCreators;
 
   beforeEach(() => {
@@ -12,7 +16,7 @@ describe("createApp", () => {
     createApp = require("../createApp").createApp;
   });
 
-  it("puts action types in the actions property", () => {
+  it("puts action types on the actions property", () => {
     const actions = { DELETE_MESSAGE: "DELETE_MESSAGE" };
     const app = createApp(mockStore, actions, [], mockSelectors);
     expect(app.actions).toEqual({
@@ -20,12 +24,19 @@ describe("createApp", () => {
     });
   });
 
-  it("bins action creators in the dispatch property", () => {
+  it("binds action creators on the dispatch property", () => {
     const actions = { deleteMessage: () => {} };
     const app = createApp(mockStore, actions, [], mockSelectors);
     expect(app.dispatch).toEqual(expect.objectContaining({
       bound: true,
     }));
     expect(app.dispatch).toEqual(expect.objectContaining(actions));
+  });
+
+  it("binds selectors on the select property", () => {
+    const app = createApp(mockStore, {}, [], mockSelectors);
+    expect(app.select).toEqual(expect.objectContaining({
+      bound: true
+    }));
   });
 });
