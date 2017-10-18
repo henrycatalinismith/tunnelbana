0487c8cf35ec34530cb5e5d84c7cfd1c4ea3aa10
Henry Catalini Smith <henry@catalinismith.se>
2017-10-18 19:43:18 +0200

    Version bump

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const { createApp } = require("./src/createApp");
 const { createMiddleware } = require("./src/createMiddleware");
 const { createReducer } = require("./src/createReducer");
 const { createSelectors } = require("./src/createSelectors");
+
+exports.createActions = createActions;
+exports.createApp = createApp;
+exports.createMiddleware = createMiddleware;
+exports.createReducer = createReducer;
+exports.createSelectors = createSelectors;
+
