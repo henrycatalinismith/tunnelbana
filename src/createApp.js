7a5059ff13fa04f488464c99325d943264d698b7
Henry Catalini Smith <henry@catalinismith.se>
2017-10-18 17:43:08 +0200

    Tidy up createApp

---
+++ src/createApp.js
@@ -0,0 +1,24 @@
+const { bindActionCreators } = require("redux");
+
+exports.createApp = (store, actions, middlewares, selectors) => {
+  const actionCreators = {};
+  const actionTypes = {};
+  Object.keys(actions).forEach(key => {
+    if (typeof actions[key] === 'function') {
+      actionCreators[key] = actions[key];
+    } else if (typeof actions[key] === 'string') {
+      actionTypes[key] = actions[key];
+    }
+  });
+
+  const app = {};
+  app.actions = actionTypes;
+  app.dispatch = bindActionCreators(actionCreators, store.dispatch);
+  app.select = selectors.bindStore(store);
+  app.before = (actionType, m) => middlewares.befores[actionType].push(m);
+  app.after = (actionType, m) => middlewares.afters[actionType].push(m);
+  app.cancel = (actionType, m) => middlewares.cancels[actionType].push(m);
+
+  return app;
+};
+
