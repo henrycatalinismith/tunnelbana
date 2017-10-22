c80b1436ab3268f61ae2f82ad3c56692fdb62b81
Henry Catalini Smith <henry@catalinismith.com>
2017-10-22 11:32:57 +0200

    import signalbox

---
+++ signalbox/index.js
@@ -0,0 +1,12 @@
+const { createActions } = require("./createActions");
+const { createApp } = require("./createApp");
+const { createMiddleware } = require("./createMiddleware");
+const { createReducer } = require("./createReducer");
+const { createSelectors } = require("./createSelectors");
+
+exports.createActions = createActions;
+exports.createApp = createApp;
+exports.createMiddleware = createMiddleware;
+exports.createReducer = createReducer;
+exports.createSelectors = createSelectors;
+
