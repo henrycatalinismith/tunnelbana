9b82c3c3364008ea36e0899a95599e3c8aa11ea6
Henry Smith <henry@henrysmith.org>
2017-10-22 21:19:01 +0200

    maybe THIS'll fix it

--- deploy.js
+++ deploy.js
@@ -19,13 +19,13 @@ const options = {
   branch: "gh-pages",
   clone: "../gh-pages",
   message: "🚂",
-  push: false,
+  //push: false,
   repo: `git@${repo}`,
   src: ["index.html", "index.css", "tunnelbana.js"]
 };
 
-ghpages.publish(".", options, () => {
-  run("git reset --soft HEAD~1");
-  run("git commit --all --amend --no-edit --reset-author --message '🚂'");
-  run(`git push --force --quiet https://${token}@${repo} master`);
-});
+ghpages.publish(".", options);
+//run("git reset --soft HEAD~1");
+//run("git commit --all --amend --no-edit --reset-author --message '🚂'");
+//run(`git push --force --quiet https://${token}@${repo} master`);
+//});
