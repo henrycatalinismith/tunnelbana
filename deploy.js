f78845a785b581f6b74ea341f967ebd503dac413
Henry Catalini Smith <henry@catalinismith.com>
2017-10-22 14:50:26 +0200

    maybe autodeploy works now

---
+++ deploy.js
@@ -0,0 +1,31 @@
+const ghpages = require("gh-pages");
+const path = require("path");
+const execSync = require("child_process").execSync;
+
+const token = process.env.GITHUB_TOKEN;
+const repo = "github.com/tunnelbana/tunnelbana.github.io.git";
+
+if (!token) {
+  process.stderr.write("Deploy failed due to missing token");
+  process.exit(-1);
+}
+
+const directory = path.join(__dirname, "gh-pages");
+const run = cmd => {
+  process.stdout.write(execSync(cmd, { cwd: directory }).toString());
+};
+
+const options = {
+  branch: "master",
+  clone: "gh-pages",
+  message: "🚂",
+  push: false,
+  repo: `git@${repo}`,
+  src: ["index.html", "index.css", "tunnelbana.js"]
+};
+
+ghpages.publish(".", options, () => {
+  run("git reset --soft HEAD~1");
+  run("git commit --all --amend --no-edit --reset-author --message '🚂'");
+  run(`git push --force --quiet https://${token}@${repo} master`);
+});
