84a8db860c0bdb4894a311a89844dff5935325b8
Henry Catalini Smith <henry@catalinismith.com>
2018-01-23 21:04:49 +0100

    react

--- entrypoints/index.js
+++ entrypoints/index.js
@@ -1,4 +1,10 @@
+const React = require("react");
+const ReactDOM = require("react-dom");
+const HighValley = require("../components").default;
+
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("highvalley");
+  const root = document.createElement("div");
+  document.body.appendChild(root);
+  ReactDOM.render(<HighValley />, root);
 });
 
