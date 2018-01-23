33516d767497195f201923430635d8563d8594c1
Henry Catalini Smith <smith@hen.cat>
2018-01-23 21:33:18 +0100

    ride of the valkyries

--- entrypoints/index.js
+++ entrypoints/index.js
@@ -1,10 +1,21 @@
 const React = require("react");
 const ReactDOM = require("react-dom");
+const { createApp } = require("signalbox");
+
 const HighValley = require("../components").default;
+const actions = require("../actions").default;
+const store = require("../reducers").default;
+const { select } = require("../reducers");
+const middlewares = require("../middlewares").default;
 
 document.addEventListener("DOMContentLoaded", () => {
+  const app = createApp(store(), actions, middlewares, select);
+
   const root = document.createElement("div");
   document.body.appendChild(root);
   ReactDOM.render(<HighValley />, root);
+
+  app.dispatch.resizeViewport(window.innerWidth, window.innerHeight);
+  console.log(app.select.viewport.dimensions());
 });
 
