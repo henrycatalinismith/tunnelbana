993b6e8e67de43e49a94c8b077e53e714a433ced
Henry Catalini Smith <henry@catalinismith.com>
2018-01-29 20:39:01 +0100

    yes

---
+++ middlewares/camera.js
@@ -0,0 +1,24 @@
+const { createMiddleware } = require("signalbox");
+const uuid = require("uuid/v1");
+
+const cube = require("../geometry/cube").default;
+const actions = require("../actions").default;
+const select = require("../reducers/selectors").default;
+
+export const middleware = createMiddleware((cancel, before, after) => ({
+  [before(actions.SELECT_HEXAGON)](store, action) {
+    const state = store.getState();
+    const hexagon = select("hexagons").from(state).byId(action.hexagon.id);
+
+    const point = cube.pixels(hexagon, 50);
+    action.camera = {
+      x: point.x,
+      y: point.y,
+    };
+    console.log(hexagon, point);
+  }
+}));
+
+export default middleware;
+
+
