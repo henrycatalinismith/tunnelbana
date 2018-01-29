72c04a1af822083000402cd1bdbab8d6798241e4
Henry Smith <henry@henrysmith.org>
2018-01-29 19:50:55 +0100

    clean

--- actions/hexagons.js
+++ actions/hexagons.js
@@ -1,11 +1,6 @@
 import { createActions } from "signalbox";
 
 export default createActions(["CREATE_HEXAGON", "SELECT_HEXAGON"], types => ({
-  createHexagon: (x, y, z) => ({
-    type: types.CREATE_HEXAGON,
-    hexagon: { x, y, z }
-  }),
-
   selectHexagon: id => ({
     type: types.SELECT_HEXAGON,
     hexagon: { id }
