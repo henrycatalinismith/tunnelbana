a714388866f70971e2389312ff6aa65a0ba84412
Henry Smith <henry@henrysmith.org>
2018-01-30 19:06:18 +0100

    bye

--- actions/index.js
+++ actions/index.js
@@ -1,11 +1,9 @@
-const actors = require("./actors").default;
 const cells = require("./cells").default;
 const hexagons = require("./hexagons").default;
 const stations = require("./stations").default;
 const viewport = require("./viewport").default;
 
 const actions = Object.assign({},
-  actors,
   cells,
   hexagons,
   stations,
