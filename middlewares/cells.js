5ae54c0946f6dc05b45b1a8361a2a63b78dd2fcd
Henry Catalini Smith <henry@catalinismith.com>
2018-01-30 20:17:30 +0100

    nicedebug

--- middlewares/cells.js
+++ middlewares/cells.js
@@ -2,11 +2,12 @@ const { createMiddleware } = require("signalbox");
 const uuid = require("uuid/v1");
 
 const actions = require("../actions").default;
+let id = 0;
 
 export const middleware = createMiddleware((cancel, before, after) => ({
   [before(actions.CREATE_CELL)](store, action) {
     if (!action.cell.id) {
-      action.cell.id = Math.random() + "";
+      action.cell.id = id++;
     }
   }
 }));
