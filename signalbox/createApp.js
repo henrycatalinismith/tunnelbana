9a685d5205e880bb5294ae985d4e9aa8d5fbf233
Henry Catalini Smith <henry@catalinismith.se>
2018-02-02 11:49:28 +0100

    Add thunks

--- signalbox/createApp.js
+++ signalbox/createApp.js
@@ -1,6 +1,6 @@
 const { bindActionCreators } = require("redux");
 
-exports.createApp = (store, actions, middlewares, selectors) => {
+exports.createApp = (store, actions, middlewares, selectors, thunks) => {
   const actionCreators = {};
   const actionTypes = {};
   Object.keys(actions).forEach(key => {
@@ -18,6 +18,7 @@ exports.createApp = (store, actions, middlewares, selectors) => {
   app.before = (actionType, m) => middlewares.befores[actionType].push(m);
   app.after = (actionType, m) => middlewares.afters[actionType].push(m);
   app.cancel = (actionType, m) => middlewares.cancels[actionType].push(m);
+  app.thunks = bindActionCreators(thunks, store.dispatch);
 
   return app;
 };
