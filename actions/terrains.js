e9cd8294e806e63a1fd23814c77431d994152ae3
Henry Catalini Smith <smith@hen.cat>
2018-02-02 12:46:10 +0100

    extremely good

--- actions/terrains.js
+++ actions/terrains.js
@@ -1,17 +1,6 @@
 import { createActions } from "signalbox";
 
-export default createActions(["CHANGE_TERRAIN"], types => ({
-  changeTerrain: (x, y, z, id) => ({
-    type: types.CHANGE_TERRAIN,
-    hexagon: { x, y, z },
-    terrain: { id },
-  }),
-
-  changeTerrainRing: (x, y, z, radius, id) => ({
-    type: types.CHANGE_TERRAIN,
-    ring: { x, y, z, radius },
-    terrain: { id },
-  }),
+export default createActions([], types => ({
 }));
 
 
