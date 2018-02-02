f155f25e90058f6c3c68193fdc5ad324d8aa4e8f
Henry Smith <henry@henrysmith.org>
2018-02-02 12:46:10 +0100

    extremely good

--- middlewares/terrains.js
+++ middlewares/terrains.js
@@ -6,34 +6,7 @@ const select = require("../reducers/selectors").default;
 const cube = require("../geometry/cube").default;
 
 export const middleware = createMiddleware((cancel, before, after) => ({
-  [before(actions.CHANGE_TERRAIN)](store, action) {
-    const state = store.getState();
-
-    if (!action.ring && !action.hexagon.id) {
-      console.log('dd');
-
-      action.hexagon = select("hexagons").from(state).at(
-        0,
-        action.hexagon.x,
-        action.hexagon.y,
-        action.hexagon.z
-      ).toJS();
-
-    }
-
-    if (action.ring) {
-      const ring = select("hexagons").from(state).ring(
-        0,
-        action.ring.x,
-        action.ring.y,
-        action.ring.z,
-        action.ring.radius
-      ).toJS();
-
-      action.ring.hexagonIds = ring;
-    }
-
-  }
+ // lol
 }));
 
 export default middleware;
