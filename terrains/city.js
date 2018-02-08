5598b9369f21d69290db7f50056a38e756785f3c
Henry Catalini Smith <henry@catalinismith.se>
2018-02-09 00:07:54 +0100

    wewee

--- terrains/city.js
+++ terrains/city.js
@@ -4,7 +4,7 @@ const cube = require("../geometry/cube").default;
 
 export const terrain = {
   id: "city",
-  color: "#b3b128",
+  color: "#a8a775",
   side: "#8e8d1d",
   height: 10,
 }
@@ -41,11 +41,21 @@ export class City extends React.PureComponent {
       .concat(points.slice(2, 5).reverse());
 
     const blocks = cube.radius(cube(), 2);
-    const buildings = blocks.map((hex, i) => {
-      const hexPx = cube.pixels(hex, buildingRadius + 2);
-      hexPx.y -= terrain.height;
-      return building(hexPx.x, hexPx.y);
-    });
+    const buildings = blocks
+      .filter(hex => {
+        if (hex.x % 2 !== 0) {
+          return false;
+        }
+        if (hex.z % 2 !== 0) {
+          return false;
+        }
+        return true;
+      })
+      .map((hex, i) => {
+        const hexPx = cube.pixels(hex, buildingRadius + 2);
+        hexPx.y -= terrain.height;
+        return building(hexPx.x, hexPx.y);
+      });
     console.log(cube.radius(cube(), 1));
 
     return [
