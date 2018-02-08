cb489d5a8a6c4c880409318422f53eaee0f41ac3
Henry Catalini Smith <henry@catalinismith.com>
2018-02-09 00:39:04 +0100

    add storybook

---
+++ stories/index.js
@@ -0,0 +1,17 @@
+const React = require("react");
+const { storiesOf, action, linkTo } = require("@storybook/react");
+const Button = require("./Button").default;
+const Welcome = require("./Welcome").default;
+
+storiesOf('Welcome', module)
+  .add('to Storybook', () => (
+    <Welcome showApp={linkTo('Button')}/>
+  ));
+
+storiesOf('Button', module)
+  .add('with text', () => (
+    <Button onClick={action('clicked')}>Hello Button</Button>
+  ))
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>
+  ));
