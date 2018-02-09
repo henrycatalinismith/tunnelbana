73fcdf42cf8457b120e7af732c40a8103780e3d5
Henry Catalini Smith <henry@catalinismith.com>
2018-02-09 22:03:25 +0100

    things have taken a bit of a weird turn tonight really

--- configuration/config.js
+++ configuration/config.js
@@ -1,4 +1,7 @@
 const { configure } = require("@storybook/react");
+const { setDefaults } = require("react-storybook-addon-props-combinations");
+
+setDefaults({ showSource: false });
 
 function loadStories() {
   require('../stories');
