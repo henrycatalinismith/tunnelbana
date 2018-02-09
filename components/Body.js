2ecb5b81b2fe84df79a7ddd80cce24223f6a5a16
Henry Smith <henry@henrysmith.org>
2018-02-09 23:43:24 +0100

    svg animation is so wonderful

--- components/Body.js
+++ components/Body.js
@@ -8,20 +8,15 @@ const { connect } = require("react-redux");
 
 export class Body extends React.PureComponent {
   static propTypes = {
-    id: PropTypes.string,
+    frame: PropTypes.string,
   };
 
   static defaultProps = {
-    id: "-x",
+    frame: "minus-x-1",
   };
 
 
   render() {
-    const style = {
-      fill: "#d8d8d8",
-      stroke: "#979797",
-    };
-
     const text = {
       transform: [
         `scale(1.6)`,
@@ -29,25 +24,41 @@ export class Body extends React.PureComponent {
       ].join(" ")
     };
 
-    const scaleX = this.props.frame === "-x" ? 1.7 : -1.7;
-
-    const transform = [
-      `scale(${scaleX} ${1.7})`,
-      `translate(${-12}, ${-30})`,
-    ].join(" ");
-
-    let bits = [
-      <ellipse key="Leg" cx="5.5" cy="33.5" rx="2.5" ry="3.5" {...style}></ellipse>,
-      <circle key="Body" cx="12.5" cy="28.5" r="9.5" {...style}></circle>,
-      <ellipse key="Leg" cx="16.5" cy="36.5" rx="2.5" ry="3.5" {...style}></ellipse>,
-      <ellipse key="Leg" transform="translate(15.883383, 28.273193) scale(-1, 1) rotate(-29.000000) translate(-15.883383, -28.273193) " cx="15.8833829" cy="28.273193" rx="2.5" ry="3.5" {...style}></ellipse>,
-      <circle key="Head" cx="12.5" cy="12.5" r="12.5" {...style}></circle>,
-      <text key="Face" x="12.5" y="12.5" {...text}>😎</text>,
-    ];
+    const transform = `translate(${-20}, ${-52})`;
 
     return (
-      <g className="Body" transform={transform}>
-        {bits}
+      <g className="Body" fill="#d8d8d8" stroke="#979797" transform={transform}>
+        <defs>
+
+          <g id="minus-x-1">
+            <ellipse id="Leg" cx="14.0999999" cy="62.3875" rx="3.0999999" ry="6.3875"></ellipse>
+            <ellipse id="Body" cx="23" cy="52.0125" rx="17.48" ry="17.3375"></ellipse>
+            <ellipse id="Leg" transform="translate(26.944607, 65.033613) rotate(13.000000) translate(-26.944607, -65.033613) " cx="26.944607" cy="65.0336126" rx="3.5999999" ry="6.3875"></ellipse>
+            <ellipse id="Leg" transform="translate(28.725425, 51.598577) scale(-1, 1) rotate(18.000000) translate(-28.725425, -51.598577) " cx="28.7254245" cy="51.5985773" rx="4.0999999" ry="6.3875"></ellipse>
+            <ellipse id="Head" cx="23" cy="23.1" rx="23" ry="22.8125"></ellipse>
+            <text key="Face" x="12" y="25.2" fontSize={29} {...text}>😎</text>
+          </g>
+
+          <g id="minus-x-2">
+            <ellipse id="Leg" transform="translate(13.022243, 61.583997) rotate(35.000000) translate(-13.022243, -61.583997) " cx="13.0222428" cy="61.583997" rx="4.0999999" ry="6.3875"></ellipse>
+            <ellipse id="Body" cx="23" cy="50.99" rx="17.48" ry="17.3375"></ellipse>
+            <ellipse id="Leg" transform="translate(30.730661, 63.270971) rotate(-22.000000) translate(-30.730661, -63.270971) " cx="30.7306614" cy="63.2709706" rx="3.5999999" ry="6.3875"></ellipse>
+            <ellipse id="Leg" transform="translate(29.225425, 51.598577) scale(-1, 1) rotate(-29.000000) translate(-29.225425, -51.598577) " cx="29.2254246" cy="51.5985773" rx="4.6" ry="6.3875"></ellipse>
+            <ellipse id="Head" cx="23" cy="22.8125" rx="23" ry="22.8125"></ellipse>
+            <text key="Face" x="12" y="25" fontSize={29} {...text}>😎</text>
+          </g>
+
+          <g id="minus-y-1">
+            <use href="#minus-x-1" transform="scale(-1, 1) translate(-45, 0)" />
+          </g>
+
+          <g id="minus-y-2">
+            <use href="#minus-x-2" transform="scale(-1, 1) translate(-45, 0)" />
+          </g>
+
+        </defs>
+
+        <use href={`#${this.props.frame}`} />
       </g>
     );
   }
--- stories/body.js
+++ stories/body.js
@@ -1,4 +1,5 @@
 const React = require("react");
+const PropTypes = require("prop-types");
 const { storiesOf, action, linkTo } = require("@storybook/react");
 //const { action } = require("@storybook/addon-actions");
 const withPropsCombinations = require("react-storybook-addon-props-combinations").default;
@@ -13,13 +14,67 @@ const Wrapper = ({ frame, width, height }) => (
   </svg>
 )
 
+class Animation extends React.PureComponent {
+  static propTypes = {
+    width: PropTypes.number,
+    height: PropTypes.number,
+    frames: PropTypes.array,
+  };
+
+  constructor(...props) {
+    super(...props);
+    this.state = {
+      frame: 0,
+    };
+  }
+
+  componentDidMount() {
+    this.interval = window.setInterval(() => {
+      const isLastFrame = this.state.frame === this.props.frames.length - 1;
+      const nextFrame = isLastFrame ? 0 : this.state.frame + 1;
+      this.setState({ frame: nextFrame });
+    }, 200);
+  }
+
+  componentWillUnmount() {
+    window.clearInterval(this.interval);
+  }
+
+  render() {
+    const { width, height, frames } = this.props;
+    const { frame } = this.state;
+    return (
+      <svg viewBox="-50 -70 100 120" style={{ height: `${height}px`, width: `${width}px`, float: "left" }}>
+        <Terrain id="grass" />
+        <Body frame={frames[frame]} />
+      </svg>
+    );
+  }
+}
+
 storiesOf("Body", module)
 
   .add("all", withPropsCombinations(Wrapper, {
     width: [200],
     height: [200],
-    frame: ["-x", "-y"],
+    frame: [
+      "minus-x-1",
+      "minus-x-2",
+      "minus-y-1",
+      "minus-y-2",
+    ],
   }))
 
-  .add("-x", () => <Wrapper frame="-x" width="300" height="300" />)
-  .add("-y", () => <Wrapper frame="-y" width="300" height="300" />)
+  .add("minus-x-1", () => <Wrapper frame="minus-x-1" width="300" height="300" />)
+  .add("minus-x-2", () => <Wrapper frame="minus-x-2" width="300" height="300" />)
+  .add("minus-x-cycle", () => (<Animation width={300} height={300} frames={[
+    "minus-x-1",
+    "minus-x-2"
+  ]} /> ))
+
+  .add("minus-y-1", () => <Wrapper frame="minus-y-1" width="300" height="300" />)
+  .add("minus-y-2", () => <Wrapper frame="minus-y-2" width="300" height="300" />)
+  .add("minus-y-cycle", () => (<Animation width={300} height={300} frames={[
+    "minus-y-1",
+    "minus-y-2"
+  ]} /> ))
