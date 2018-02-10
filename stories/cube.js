8e39dba397b6b823a8dc19a814b7247e93c25b1d
Henry Smith <henry@henrysmith.org>
2018-02-10 19:15:27 +0100

    fix hex

--- stories/cube.js
+++ stories/cube.js
@@ -6,21 +6,30 @@ const Story = require("./Story").default;
 const Hexagon = require("../components/Hexagon").default;
 const Cube = require("../components/Cube").default;
 
+const Hex = ({ x, y, z, ...props }) => (
+  <Cube key={[x, y, z].join(",")} x={x} y={y} z={z}>
+    <Hexagon fill="#f4f4f1" stroke="#999" {...props} />
+    <text key="x" fontFamily="sans-serif" fill="#59b30c" x={24} y={-16}>{x}</text>,
+    <text key="y" fontFamily="sans-serif" fill="#e85ee6" x={-28} y={-16}>{y}</text>,
+    <text key="z" fontFamily="sans-serif" fill="#3199e7" x={-4} y={40}>{z}</text>,
+  </Cube>
+);
+
 storiesOf("Cube", module)
 
   .add("center", () => (
     <Story>
-      <Cube x={0} y={0} z={0}><Hexagon stroke="black" /></Cube>
+      <Hex x={0} y={0} z={0} />
     </Story>
   ))
 
   .add("line", () => (
     <Story>
       {cube.radius(cube(), 2).map(({ x, y, z}, i) => (
-        <Cube key={i} x={x} y={y} z={z}><Hexagon stroke="black" /></Cube>
+        <Hex key={i} x={x} y={y} z={z} />
       ))}
       {cube.line(cube(-2, 0, 2), cube(2, 0, -2)).map(({ x, y, z}, i) => (
-        <Cube key={i} x={x} y={y} z={z}><Hexagon fill="black" /></Cube>
+        <Hex key={i} x={x} y={y} z={z} fill="#e2e7eb" />
       ))}
     </Story>
   ))
@@ -28,11 +37,12 @@ storiesOf("Cube", module)
   .add("neigbors", () => (
     <Story>
       {cube.radius(cube(), 2).map(({ x, y, z}, i) => (
-        <Cube key={i} x={x} y={y} z={z}><Hexagon stroke="black" /></Cube>
+        <Hex key={i} x={x} y={y} z={z} />
       ))}
       <Cube x={0} y={1} z={-1}><Hexagon fill="red" /></Cube>
+      <Hex x={0} y={1} z={-1} fill="#c2d7d0" />
       {cube.neighbors(cube(0, 1, -1)).map(({ x, y, z}, i) => (
-        <Cube key={i} x={x} y={y} z={z}><Hexagon fill="blue" /></Cube>
+        <Hex key={i} x={x} y={y} z={z} fill="#e2e7eb" />
       ))}
     </Story>
   ))
@@ -40,7 +50,7 @@ storiesOf("Cube", module)
   .add("radius", () => (
     <Story>
       {cube.radius(cube(), 2).map(({ x, y, z}, i) => (
-        <Cube key={i} x={x} y={y} z={z}><Hexagon stroke="black" /></Cube>
+        <Hex key={i} x={x} y={y} z={z} />
       ))}
     </Story>
   ))
@@ -48,7 +58,7 @@ storiesOf("Cube", module)
   .add("ring", () => (
     <Story>
       {cube.ring(cube(), 2).map(({ x, y, z}, i) => (
-        <Cube key={i} x={x} y={y} z={z}><Hexagon stroke="black" /></Cube>
+        <Hex key={i} x={x} y={y} z={z} />
       ))}
     </Story>
   ))
