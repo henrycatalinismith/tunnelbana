149abcc223f38d212caefd437bf41599670af6bf
Henry Smith <henry@henrysmith.org>
2018-02-10 15:44:35 +0100

    explain

--- configuration/config.js
+++ configuration/config.js
@@ -1,3 +1,11 @@
+// Storybook's Config File
+// =======================
+//
+// This is here under the name "config.js" because Storybook only lets you
+// configure the *directory* containing its config file, not the name of the
+// file itself. I wanted to keep all the config together and this weird
+// ambiguous filename is a compromise I had to make to get that.
+
 const { configure } = require("@storybook/react");
 const { setDefaults } = require("react-storybook-addon-props-combinations");
 require("@storybook/addon-console");
