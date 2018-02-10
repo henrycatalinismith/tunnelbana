9e42d5cc8b46d0743e74b46d3cfe7b27d12c12ce
Henry Catalini Smith <smith@hen.cat>
2018-02-10 04:27:17 +0100

    yes

---
+++ configuration/constants.js
@@ -0,0 +1,25 @@
+export const terrains = {
+  city: {
+    color: "#a8a775",
+    cliff: "#8e8d1d",
+    height: 10,
+  },
+
+  forest: {
+    color: "#152d07",
+    cliff: "#071a04",
+    height: 18,
+  },
+
+  grass: {
+    color: "#b3b128",
+    cliff: "#8e8d1d",
+    height: 10,
+  },
+
+  water: {
+    color: "#2369a6",
+    cliff: "#174d7c",
+    height: 2,
+  },
+};
