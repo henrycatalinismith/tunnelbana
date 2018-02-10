2753cee5f77e4397b462eaa342b6d065c8996479
Henry Catalini Smith <smith@hen.cat>
2018-02-10 15:42:13 +0100

    fix errors

--- stories/cube.js
+++ stories/cube.js
@@ -16,16 +16,16 @@ storiesOf("Cube", module)
 
   .add("radius", () => (
     <Story>
-      {cube.radius(cube(), 2).map(({ x, y, z}) => (
-        <Cube x={x} y={y} z={z}><Hexagon stroke="black" /></Cube>
+      {cube.radius(cube(), 2).map(({ x, y, z}, i) => (
+        <Cube key={i} x={x} y={y} z={z}><Hexagon stroke="black" /></Cube>
       ))}
     </Story>
   ))
 
   .add("ring", () => (
     <Story>
-      {cube.ring(cube(), 2).map(({ x, y, z}) => (
-        <Cube x={x} y={y} z={z}><Hexagon stroke="black" /></Cube>
+      {cube.ring(cube(), 2).map(({ x, y, z}, i) => (
+        <Cube key={i} x={x} y={y} z={z}><Hexagon stroke="black" /></Cube>
       ))}
     </Story>
   ))
