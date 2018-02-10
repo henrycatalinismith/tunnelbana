87da5780d7a26524423e4012241d6f2f8ae6ed00
Henry Catalini Smith <henry@catalinismith.se>
2018-02-10 15:39:06 +0100

    nice

--- configuration/config.js
+++ configuration/config.js
@@ -1,5 +1,6 @@
 const { configure } = require("@storybook/react");
 const { setDefaults } = require("react-storybook-addon-props-combinations");
+require("@storybook/addon-console");
 
 setDefaults({ showSource: false });
 
