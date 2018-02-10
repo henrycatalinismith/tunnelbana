a4b68b574d0290bd01ce43ed3a34e7108d38e4a3
Henry Catalini Smith <henry@catalinismith.se>
2018-02-10 01:31:35 +0100

    ffffff

---
+++ stories/cube.js
@@ -0,0 +1,32 @@
+const React = require("react");
+const { storiesOf, action, linkTo } = require("@storybook/react");
+
+const cube = require("../geometry/cube").default;
+const { Hexagon } = require("../components/Hexagon");
+const { Cube } = require("../components/Cube");
+
+storiesOf("Cube", module)
+
+  .add("center", () => (
+    <svg viewBox="-200 -200 1000 1000" width="100%" height="100%">
+      <Cube x={0} y={0} z={0}><Hexagon /></Cube>
+    </svg>
+  ))
+
+  .add("radius", () => (
+    <svg viewBox="-300 -300 1000 1000" width="100%" height="100%">
+      {cube.radius(cube(), 2).map(({ x, y, z}) => (
+        <Cube x={x} y={y} z={z}><Hexagon /></Cube>
+      ))}
+    </svg>
+  ))
+
+  .add("ring", () => (
+    <svg viewBox="-300 -300 1000 1000" width="100%" height="100%">
+      {cube.ring(cube(), 2).map(({ x, y, z}) => (
+        <Cube x={x} y={y} z={z}><Hexagon /></Cube>
+      ))}
+    </svg>
+  ))
+
+
