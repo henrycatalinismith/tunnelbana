8388611d2717d4437e9ed91a6095566bdcd25b0c
Henry Catalini Smith <henry@catalinismith.se>
2018-02-10 14:26:59 +0100

    lol

--- stories/cliff.js
+++ stories/cliff.js
@@ -7,19 +7,19 @@ const { Cliff } = require("../components/Cliff");
 storiesOf("Cliff", module)
 
   .add("stroke", () => (
-    <svg viewBox="-100 -100 300 320" width="100%" height="100%">
+    <svg viewBox="-100 -100 300 320" width="50%" height="100%">
       <Cliff radius={100} stroke="red" />
     </svg>
   ))
 
   .add("strokeWidth", () => (
-    <svg viewBox="-100 -100 300 320" width="100%" height="100%">
+    <svg viewBox="-100 -100 300 320" width="50%" height="100%">
       <Cliff radius={80} stroke="red" strokeWidth={20} />
     </svg>
   ))
 
   .add("fill", () => (
-    <svg viewBox="-100 -100 300 320" width="100%" height="100%">
+    <svg viewBox="-100 -100 300 320" width="50%" height="100%">
       <Cliff radius={80} stroke="red" fill="red" />
     </svg>
   ))
