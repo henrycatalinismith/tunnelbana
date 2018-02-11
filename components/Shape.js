ee135c6b7c0128550c7b43ff0ead968e22820003
Henry Smith <henry@henrysmith.org>
2018-02-12 00:02:52 +0100

    omg

--- components/Shape.js
+++ components/Shape.js
@@ -1,33 +1,14 @@
 const React = require("react");
 const PropTypes = require("prop-types");
 
-const contextProps = {
-  fill: "fillStyle",
-  stroke: "strokeStyle",
-  strokeWidth: "strokeWidth",
-};
-
 export default class Shape extends React.PureComponent {
   static propTypes = {
     context: PropTypes.object,
-    fill: PropTypes.string,
-    stroke: PropTypes.string,
-    strokeWidth: PropTypes.number,
-  };
-
-  static defaultProps = {
-    fill: undefined,
-    stroke: "none",
-    strokeWidth: 0,
   };
 
   componentWillReceiveProps(nextProps) {
     if (this.props.context || !nextProps.context) {
       return;
-    }
-
-    for (let svgName in contextProps) {
-      nextProps.context[contextProps[svgName]] = this.props[svgName];
     }
 
     this.draw(nextProps.context);
